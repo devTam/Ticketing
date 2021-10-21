67517414ca9c747791da01927f0a6e47f2b94e54
devTam <tammy.batubo@gmail.com>
2021-10-21 02:34:22 +0100

    create mongoDb instance with deployment and service and connect mongoose to db

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,25 +1,31 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/requestValidationError';
-import { DbConnectionError } from '../errors/dbConnectionError';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/requestValidationError";
+import { DbConnectionError } from "../errors/dbConnectionError";
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
-    body('email').isEmail().withMessage('Please provide a valid Email!'),
-    body('password').trim().isLength({ min: 7 }).withMessage('Password must be at least 7 characters')
-], 
-(req: Request, res: Response) => {
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Please provide a valid Email!"),
+    body("password")
+      .trim()
+      .isLength({ min: 7 })
+      .withMessage("Password must be at least 7 characters"),
+  ],
+  (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
 
-    console.log('creating a user');
+    console.log("creating a user");
     res.send({});
-});
+  }
+);
 
 export { router as signupRouter };
